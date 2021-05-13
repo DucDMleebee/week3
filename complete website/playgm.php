6f521a15da1aff644dd423b5c9a75352131e2633
Nguyễn Cao Huy Hoàng <61876488+antoinenguyen-09@users.noreply.github.com>
2021-05-14 01:27:41 +0700

    Update playgm.php

--- complete website/playgm.php
+++ complete website/playgm.php
@@ -1,3 +1,14 @@
-<?php 
-echo 'Playing game (only for student)';
+<?php
+session_start();
+if(isset($_SESSION['teacher']) || isset($_SESSION['student'])){
+    if(!$_SESSION['student']){   // only student can access.
+        // Redirect them to the  page
+        header("Location: creategm.php");  
+    }
+    else{
+        echo 'Playing game (only for student)';  // chèn nội dung vào đây
+    }
+} else{
+    header("Location: login.php");
+} 
 ?>
