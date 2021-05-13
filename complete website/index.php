e83af3606462bc0bfd3cc4de2f39680117841c42
Nguyễn Cao Huy Hoàng <61876488+antoinenguyen-09@users.noreply.github.com>
2021-05-14 00:45:00 +0700

    Update index.php

--- complete website/index.php
+++ complete website/index.php
@@ -1,12 +1,13 @@
 <?php
 session_start();
 
-if(isset($_SESSION['teacher']) || isset($_SESSION['student'])){
-    echo "This is index";
-} 
-else {
+if(!isset($_SESSION['teacher']) || !isset($_SESSION['student'])){
     // Redirect them to the login page
-    header("Location: login.php");
+    header("Location: login.php");  
+} 
+else{
+    $create = "creategm.php";
+    $play = "playgm.php";
 } 
 ?>
 <!DOCTYPE html>
@@ -14,10 +15,15 @@ else {
 <head>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
-<body>
+<body>  ​
     <a href='list.php'><button type="button" class="btn btn-secondary btn-lg btn-block btn-success">List of students</button></a>
     <a href='assign.php'><button type="button" class="btn btn-secondary btn-lg btn-block btn-warning">Assignments</button>
-    <a href='game.php'><button type="button" class="btn btn-secondary btn-lg btn-block btn-info">Games</button>
+    <a id="game" href='#'><button onclick="game()" type="button" class="btn btn-secondary btn-lg btn-block btn-info">Games</button>
     <a href='signout.php'><button type="button" class="btn btn-secondary btn-lg btn-block btn-dark">Sign out</button>
+    <script> 
+    function game() {
+        var page = "<?php if($_SESSION['teacher']) echo $create; else if($_SESSION['student']) echo $play; else echo '#'; ?>";
+        document.querySelector("#game").href = page;    
+    } 
+    </script>
 </body>
-
