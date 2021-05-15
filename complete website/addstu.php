f2111e53aae48e99792a6bb5aaecd5dedd83ffc5
Nguyễn Cao Huy Hoàng <61876488+antoinenguyen-09@users.noreply.github.com>
2021-05-15 22:04:10 +0700

    Update addstu.php

--- complete website/addstu.php
+++ complete website/addstu.php
@@ -1,4 +1,8 @@
 <?php
-
-   echo 'Create a student (only for teacher)';
+   session_start();
+   if(!isset($_SESSION['teacher']) || !$_SESSION['teacher']){
+      header('Location: login.php');
+   } else{
+      echo 'Create a student (only for teacher)';
+   }
 ?>
