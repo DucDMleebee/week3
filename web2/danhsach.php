e21314c93e2030d329f99405122b643b0bd97fe9
DucDMleebee <83963452+DucDMleebee@users.noreply.github.com>
2021-05-16 22:05:57 +0700

    Add files via upload

--- web2/danhsach.php
+++ web2/danhsach.php
@@ -3,10 +3,10 @@
 $fileList = glob('baitap/*');
 foreach($fileList as $filename){
     if(is_file($filename)){
-	$target = basename("$filename").PHP_EOL;
+	$target = basename("$filename",".pdf").PHP_EOL;
 	$mypath="/var/www/html/web2/baitap/$target";
 	mkdir($mypath, 0777, true);
-        echo $target . '<a href="' . $filename . '"> Download </a>' . '<a href=submit.php>Nop bai</a>', '<br>';
+        echo $target . '<a href="' . $filename . '"> Download </a>' . '<a href=submit.php> Nop bai </a>' . '<a href=submitted.php> Bai da nop </a>', '<br>';
     }   
 }
 ?>
