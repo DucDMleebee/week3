430659c4042e2204bda83a13899a3b88fd60a9c3
DucDMleebee <83963452+DucDMleebee@users.noreply.github.com>
2021-05-16 22:43:10 +0700

    Add files via upload

--- web2/answer.php
+++ web2/answer.php
@@ -1,12 +1,41 @@
 <html>
 <body>
 <?php
+$servername = "localhost";
+$username = "kali";
+$password = "kali";
+$dbname = "class";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT hints from challenge ORDER BY chalID DESC LIMIT 1";
+mysqli_query($conn, $sql);
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+  // output data of each row
+  while($row = mysqli_fetch_assoc($result)) {
+    echo "hints: " . $row["hints"];
+  }
+} else {
+  echo "0 results";
+}
+
+mysqli_close($conn);
+?>
+<?php
 if (isset($_POST["btn_submit"])) {
 $answer = $_POST["answer"];
 		if ($answer == "") {
 			echo "Answer is empty!";
 		}else{
-			$myfile = fopen("challenge/$answer.txt", "r") or die("Wrong answer");
+			$myfile = fopen("challenge/$answer.txt", "r") or die("<br>Wrong answer");
+			echo "<br>Correct<br>";
 			echo fread($myfile,filesize("challenge/$answer.txt"));
 			fclose($myfile);
 		}
