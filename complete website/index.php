a0ed048e9b36b9c1c4a79eadbcf9c07c18743a4e
Nguyễn Cao Huy Hoàng <61876488+antoinenguyen-09@users.noreply.github.com>
2021-05-16 21:53:34 +0700

    Update index.php

--- complete website/index.php
+++ complete website/index.php
@@ -15,11 +15,12 @@ else{
 <head>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
-<body>  ​
+<body>  
     <a href='list.php'><button type="button" class="btn btn-secondary btn-lg btn-block btn-success">List of students</button></a>
     <a href='assign.php'><button type="button" class="btn btn-secondary btn-lg btn-block btn-warning">Assignments</button></a>
     <a id="game" href='#'><button onclick="game()" type="button" class="btn btn-secondary btn-lg btn-block btn-info">Games</button></a>
     <?php if($_SESSION['teacher']) echo '<a href="addstu.php"><button type="button" class="btn btn-secondary btn-lg btn-block btn-danger">Add a student</button></a>'; ?>
+    <?php if($_SESSION['student']) echo '<a href="view_mess.php"><button type="button" class="btn btn-secondary btn-lg btn-block btn-danger">View message</button></a>'; ?>
     <a href='signout.php'><button type="button" class="btn btn-secondary btn-lg btn-block btn-dark">Sign out</button></a>
     <script> 
     function game() {
@@ -27,5 +28,6 @@ else{
         document.querySelector("#game").href = page;    
     } 
     </script>
+
 </body>
 
