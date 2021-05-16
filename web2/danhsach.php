e1d720f30d40a0954dab31a2e52dce92cd26add5
DucDMleebee <83963452+DucDMleebee@users.noreply.github.com>
2021-05-16 10:18:49 +0700

    Update danhsach.php

--- web2/danhsach.php
+++ web2/danhsach.php
@@ -3,7 +3,10 @@
 $fileList = glob('baitap/*');
 foreach($fileList as $filename){
     if(is_file($filename)){
-        echo $filename . '<a href="' . $filename . '"> Download </a>', '<br>'; 
+	$target = basename("$filename").PHP_EOL;
+	$mypath="/var/www/html/web2/baitap/$target";
+	mkdir($mypath, 0777, true);
+        echo $target . '<a href="' . $filename . '"> Download </a>' . '<a href=submit.php>Nop bai</a>', '<br>';
     }   
 }
 ?>
