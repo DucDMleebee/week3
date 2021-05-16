01e4f30a256e3974239c812e896698299d479c26
DucDMleebee <83963452+DucDMleebee@users.noreply.github.com>
2021-05-16 10:19:36 +0700

    Add files via upload

---
+++ web2/submit.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="uploadanswer.php" method="post" enctype="multipart/form-data">
+  Chon bai tap de nop:
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="Upload File" name="submit">
+</form>
+
+</body>
+</html>
